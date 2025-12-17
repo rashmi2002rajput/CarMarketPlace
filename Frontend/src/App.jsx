bfac33eb7e7afd9c75ed82f18af2221660d2d5fd
Rashmi Rajput <1980@techno.local>
2025-12-17 10:58:30 +0530

    First Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h6>hello world</h6>
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default App
